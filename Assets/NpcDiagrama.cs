ad9f8ad7ea782f5b6630aad1ceb0858fe20e0dad
Alafresh <51685896+Alafresh@users.noreply.github.com>
2022-11-02 23:04:24 -0500

    update all

--- Assets/NpcDiagrama.cs
+++ Assets/NpcDiagrama.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class NpcDiagrama : MonoBehaviour
 {
     [SerializeField] GameObject diagrama;
+    [SerializeField] GameObject continuar;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "player")
         {
             diagrama.SetActive(true);
+            continuar.SetActive(true);
             other.gameObject.SetActive(false);
         }
     }
